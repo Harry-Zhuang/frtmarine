f1c130dcf60c19bf28b0df64342525eb29b40a00
Matthew <matthew.fan@outlook.co.nz>
2023-03-19 20:36:30 +1300

    Updated content

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
       <header className="App-header">
         <img src="/favicon.ico" className="App-logo" alt="logo" />
         <p>Welcome to FRT Marine</p>
-        <p>Here's when magic begins......</p>
+        <p>Here's when magic begins...</p>
       </header>
     </div>
   );
